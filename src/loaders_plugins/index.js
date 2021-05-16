4ffb41e3905f9dbdfbf944f617a2240aaf3b2978
Minwei Shen <minweishen1991@gmail.com>
2021-05-16 15:33:22 -0700

    some optimization

--- src/loaders_plugins/index.js
+++ src/loaders_plugins/index.js
@@ -3,6 +3,11 @@ import webpack1 from './assets/webpack1.png';
 import webpack2 from './assets/webpack2.jpeg';
 import sample from './assets/sample.txt';
 
+// need to use import isFunction from 'lodash/isFunction' to enable tree-shaking
+// however, the babel-plugin-lodash will help us do this
+// we can potentially use https://github.com/lodash/lodash-webpack-plugin to remove even more code!!!
+import { isFunction } from 'lodash'; 
+
 [webpack1, webpack2].map(src => {
   const img = new Image();
   img.src = src;
@@ -10,3 +15,5 @@ import sample from './assets/sample.txt';
 }).forEach(img => document.appendChild(img));
 
 console.log(sample);
+
+console.log(isFunction(() => {}))
